75ab0105e030baf53b6638d2989e66d5ea105ce6
HungNDHE153622 <HungNDHE153622@fpt.edu.vn>
2024-07-04 20:50:01 +0700

    feat:Make api review

---
+++ API/Services/IReviewService.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+
+namespace API.Services
+{
+    public interface IReviewService
+    {
+        //tim kiem tat ca cac Review
+        Task<List<Review>> GetAllReviewAsync();
+
+        //tim kiem Review theo id
+        Task<Review> FindReviewWithItsId(int Id);
+        //them 1 service moi
+        Task<Review> CreateReviewAsync(Service services);
+        //edit 1 service 
+        Task<Review> EditReviewAsync(int Id, Service services);
+        //xoa 1 service
+        Task<Review> DeleteReviewAsync(int Id);
+    }
+}
