6e37ec5bbec638dd6f2a6e063fe05ee8896340dc
TruongNQHE140426 <truongnqhe140426@fpt.edu.vn>
2024-08-01 04:18:17 +0700

    fix: fix system setting

--- Web/Models/SettingViewModel.cs
+++ Web/Models/SettingViewModel.cs
@@ -2,5 +2,9 @@
 {
     public class SettingViewModel
     {
-    }
+		public int Id { get; set; }
+		public string Key { get; set; } = null!;
+		public string Value { get; set; } = null!;
+		public string? Description { get; set; }
+	}
 }
