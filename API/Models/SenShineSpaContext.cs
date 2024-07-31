097cd30329630d8ccb9896699de6d4549b8dabf1
DuyNNHE150074 <duynnhe150074@fpt.edu.vn>
2024-08-01 01:02:25 +0700

    fix: fix context

--- API/Models/SenShineSpaContext.cs
+++ API/Models/SenShineSpaContext.cs
@@ -44,15 +44,6 @@ namespace API.Models
         public virtual DbSet<Ward> Wards { get; set; } = null!;
         public virtual DbSet<WorkSchedule> WorkSchedules { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=DESKTOP-SF3JEMS\\SQLEXPRESS;database=SenShineSpa;uid=sa;pwd=1;TrustServerCertificate=true;Trusted_Connection=True;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<AdministrativeRegion>(entity =>
