14c8467d61bd7d7b6ba3813d6350a9b7e7d80197
DuyNNHE150074 <duynnhe150074@fpt.edu.vn>
2024-08-01 16:49:56 +0700

    feat: add salary service and interface

---
+++ API/Services/ISalaryService.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+
+namespace API.Services
+{
+    public interface ISalaryService
+    {
+        Task<Salary> CreateSalary(Salary salary);
+        ICollection<Salary> GetSalaries();
+        ICollection<Salary> GetSalariesByMonthAndYear(int month, int year);
+        Salary GetSalary(int id);
+        Task<Salary> UpdateSalary(int id, Salary salary);
+        Task<bool> DeleteSalary(int id);
+        bool SalaryExist(int id);
+    }
+}
---
+++ API/Services/Impl/SalaryService.cs
@@ -0,0 +1,63 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services.Impl
+{
+    public class SalaryService : ISalaryService
+    {
+        private readonly SenShineSpaContext _context;
+
+        public SalaryService(SenShineSpaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Salary> CreateSalary(Salary salary)
+        {
+            await _context.Salaries.AddAsync(salary);
+            await _context.SaveChangesAsync();
+
+            return salary;
+        }
+
+        public ICollection<Salary> GetSalaries()
+        {
+            return _context.Salaries.ToList();
+        }
+
+        public ICollection<Salary> GetSalariesByMonthAndYear(int month, int year)
+        {
+            return _context.Salaries.Where(s => s.SalaryMonth == month && s.SalaryYear == year).ToList();
+        }
+
+        public Salary GetSalary(int id)
+        {
+            var salaries = GetSalaries();
+            return salaries.FirstOrDefault(b => b.Id == id);
+        }
+        public async Task<Salary> UpdateSalary(int id, Salary salary)
+        {
+            var salaryUpdate = await _context.Salaries.FirstOrDefaultAsync(s => s.Id == id);
+            await _context.SaveChangesAsync();
+
+            return salaryUpdate;
+        }
+
+        public async Task<bool> DeleteSalary(int id)
+        {
+            var salary = await _context.Salaries.FindAsync(id);
+            if (salary == null)
+            {
+                return false;
+            }
+            _context.Salaries.Remove(salary);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public bool SalaryExist(int id)
+        {
+            return _context.Salaries.Any(s => s.Id == id);
+        }
+    }
+}
