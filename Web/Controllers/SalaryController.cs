a727a720765e67dd48159b0bf3667e4deb6c1d83
DuyNNHE150074 <duynnhe150074@fpt.edu.vn>
2024-08-02 23:41:53 +0700

    feat: add salary web

---
+++ Web/Controllers/SalaryController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class SalaryController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:5297/api");
+        private readonly HttpClient _client;
+
+        public SalaryController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ListSalary()
+        {
+            List<SalaryViewModel> salaries = new List<SalaryViewModel>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Salary/GetAll").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                salaries = JsonConvert.DeserializeObject<List<SalaryViewModel>>(data);
+            }
+
+            return View(salaries);
+        }
+
+        [HttpGet]
+        public IActionResult CreateSalary()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSalary(SalaryViewModel salary)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(salary);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await _client.PostAsync(_client.BaseAddress + "/Salary/Create", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("ListSalary");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Error");
+                    return View(salary);
+                }
+            }
+
+            return View(salary);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateSalary(int id)
+        {
+            SalaryViewModel salary = null;
+            HttpResponseMessage response = await _client.GetAsync(_client.BaseAddress + "/Salary/GetById?id=" + id);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                salary = JsonConvert.DeserializeObject<SalaryViewModel>(data);
+            }
+
+            if (salary == null)
+            {
+                return NotFound("salary không tồn tại");
+            }
+
+            return View(salary);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateSalary(SalaryViewModel salary)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(salary);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await _client.PutAsync(_client.BaseAddress + "/Salary/Update?id=" + salary.Id, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("ListSalary");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Có lỗi xảy ra khi cập nhật salary");
+                    return View(salary);
+                }
+            }
+
+            return View(salary);
+        }
+    }
+}
---
+++ Web/Models/SalaryViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.Models
+{
+    public class SalaryViewModel
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public decimal? BaseSalary { get; set; }
+        public decimal? Allowances { get; set; }
+        public decimal? Bonus { get; set; }
+        public decimal? Deductions { get; set; }
+        public decimal? TotalSalary { get; set; }
+        public int? SalaryMonth { get; set; }
+        public int? SalaryYear { get; set; }
+    }
+}
