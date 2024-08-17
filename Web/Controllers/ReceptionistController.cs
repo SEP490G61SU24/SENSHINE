54f397c7a559560917e67a42086b2f9574a92bf3
HungNDHE153622 <HungNDHE153622@fpt.edu.vn>
2024-08-17 21:54:22 +0700

    feat:update UI home new and new

--- Web/Controllers/ReceptionistController.cs
+++ Web/Controllers/ReceptionistController.cs
@@ -1,12 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
-    public class ReceptionistController : Controller
+    public class ReceptionistController : BaseController
     {
-        public IActionResult Dashboard()
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly ILogger<UserController> _logger;
+
+
+        public ReceptionistController(IConfiguration configuration, IHttpClientFactory clientFactory, ILogger<UserController> logger) : base(configuration, clientFactory, logger)
         {
-            return View();
+            _configuration = configuration;
+            _clientFactory = clientFactory;
+            _logger = logger;
+        }
+        private async Task<UserViewModel> LoadUserAsync()
+        {
+            var user = new UserViewModel();
+            var token = HttpContext.Session.GetString("Token");
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                var userProfile = await GetUserProfileAsync(token);
+
+                if (userProfile != null)
+                {
+                    user.Id = userProfile.Id;
+                    user.UserName = userProfile.UserName;
+                    user.FirstName = userProfile.FirstName;
+                    user.MidName = userProfile.MidName;
+                    user.LastName = userProfile.LastName;
+                    user.Phone = userProfile.Phone;
+                    user.BirthDate = userProfile.BirthDate;
+                    user.Status = userProfile.Status;
+                    user.StatusWorking = userProfile.StatusWorking;
+                    user.SpaId = userProfile.SpaId;
+                    user.ProvinceCode = userProfile.ProvinceCode;
+                    user.DistrictCode = userProfile.DistrictCode;
+                    user.WardCode = userProfile.WardCode;
+                    user.Address = userProfile.Address;
+                    user.Roles = userProfile.Roles;
+                    user.RoleName = userProfile.RoleName;
+                    user.RoleId = userProfile.RoleId;
+                    user.FullName = $"{userProfile.FirstName} {userProfile.MidName} {userProfile.LastName}";
+                }
+                else
+                {
+                    ViewData["Error"] = "Failed to retrieve user profile.";
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "An error occurred while fetching the user profile.");
+            }
+
+            return user;
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            var user = await LoadUserAsync();
+            return View(user);
         }
     }
 }
