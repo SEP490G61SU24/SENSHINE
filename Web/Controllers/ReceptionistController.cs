954609f3d4d7d1fbcad2e954851556a15575a685
HungNDHE153622 <HungNDHE153622@fpt.edu.vn>
2024-08-18 19:27:45 +0700

    fix:fix controller dashboard

--- Web/Controllers/ReceptionistController.cs
+++ Web/Controllers/ReceptionistController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Dtos;
+using Microsoft.AspNetCore.Mvc;
 using Web.Models;
 
 namespace Web.Controllers
@@ -16,53 +17,16 @@ namespace Web.Controllers
             _clientFactory = clientFactory;
             _logger = logger;
         }
-        private async Task<UserViewModel> LoadUserAsync()
-        {
-            var user = new UserViewModel();
-            var token = HttpContext.Session.GetString("Token");
-
-            if (!string.IsNullOrEmpty(token))
-            {
-                var userProfile = await GetUserProfileAsync(token);
-
-                if (userProfile != null)
-                {
-                    user.Id = userProfile.Id;
-                    user.UserName = userProfile.UserName;
-                    user.FirstName = userProfile.FirstName;
-                    user.MidName = userProfile.MidName;
-                    user.LastName = userProfile.LastName;
-                    user.Phone = userProfile.Phone;
-                    user.BirthDate = userProfile.BirthDate;
-                    user.Status = userProfile.Status;
-                    user.StatusWorking = userProfile.StatusWorking;
-                    user.SpaId = userProfile.SpaId;
-                    user.ProvinceCode = userProfile.ProvinceCode;
-                    user.DistrictCode = userProfile.DistrictCode;
-                    user.WardCode = userProfile.WardCode;
-                    user.Address = userProfile.Address;
-                    user.Roles = userProfile.Roles;
-                    user.RoleName = userProfile.RoleName;
-                    user.RoleId = userProfile.RoleId;
-                    user.FullName = $"{userProfile.FirstName} {userProfile.MidName} {userProfile.LastName}";
-                }
-                else
-                {
-                    ViewData["Error"] = "Failed to retrieve user profile.";
-                }
-            }
-            else
-            {
-                ModelState.AddModelError(string.Empty, "An error occurred while fetching the user profile.");
-            }
-
-            return user;
-        }
+        
 
         public async Task<IActionResult> Dashboard()
         {
-            var user = await LoadUserAsync();
-            return View(user);
+            UserDTO userProfile = ViewData["UserProfile"] as UserDTO;
+            if (userProfile == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+            return View();
         }
     }
 }
