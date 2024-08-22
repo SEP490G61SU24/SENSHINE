21a8151c9a045649c78bd33990f1bfe90625af1c
Kytade160671 <kytade160671@fpt.edu.vn>
2024-08-22 18:04:29 +0700

    fix:fix

--- Web/Controllers/InvoiceController.cs
+++ Web/Controllers/InvoiceController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http;
-using System.Runtime.Intrinsics.X86;
 using System.Text;
 using Web.Models;
 
