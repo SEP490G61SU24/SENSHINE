f6e79e2add15d0650f5b427f89f8c3b35e92a545
Kytade160671 <kytade160671@fpt.edu.vn>
2024-08-26 04:07:35 +0700

    fix:fix

--- API/Services/IUserService.cs
+++ API/Services/IUserService.cs
@@ -15,6 +15,7 @@ namespace API.Services
         Task<UserDTO> GetById(int id);
         Task<UserDTO> GetByUserName(string username);
         Task<IEnumerable<UserDTO>> GetUsersByRole(int roleId);
+        Task<IEnumerable<UserDTO>> GetCustomerByName(string name);
         Task<PaginatedList<UserDTO>> GetUsersByRoleWithPage(int roleId, int pageIndex, int pageSize, string searchTerm, string spaId);
         Task<string> GetAddress(string wardCode, string districtCode, string provinceCode);
         Task<bool> ChangePassword(string userName, string currentPassword, string newPassword, bool userChange);
