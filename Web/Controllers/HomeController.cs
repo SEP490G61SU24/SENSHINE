a7de873abf717f617b9dcd179a8663d9399d46de
HungNDHE153622 <HungNDHE153622@fpt.edu.vn>
2024-08-25 21:02:44 +0700

    fix:fix UI home page

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,90 +1,102 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 using Newtonsoft.Json;
 using System.Diagnostics;
-using System.Net.Http;
 using Web.Models;
 
-namespace Web.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly HttpClient _httpClient;
-        private readonly IConfiguration _configuration;
-        
-        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        _logger = logger;
+        _configuration = configuration;
+        var apiUrl = _configuration.GetValue<string>("ApiUrl");
+        _httpClient = new HttpClient { BaseAddress = new Uri(apiUrl) };
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [Route("/")]
+    public async Task<IActionResult> HomePageAsync()
+    {
+        List<NewsViewModel> viewList = new List<NewsViewModel>();
+        HttpResponseMessage response = await _httpClient.GetAsync("/api/ListNewsSortDESCByNewDate");
+
+        if (response.IsSuccessStatusCode)
         {
-            _logger = logger;
-            _configuration = configuration;
-            var apiUrl = _configuration.GetValue<string>("ApiUrl");
-            _httpClient = new HttpClient { BaseAddress = new Uri(apiUrl) };
+            string data = await response.Content.ReadAsStringAsync();
+            viewList = JsonConvert.DeserializeObject<List<NewsViewModel>>(data);
+        }
+        else
+        {
+            // Log error message here
+            ModelState.AddModelError(string.Empty, "An error occurred while fetching the news list.");
         }
 
-        public IActionResult Index()
+        return View(viewList);
+    }
+
+    [Route("Home/Public/[action]")]
+    public async Task<IActionResult> NewsPage()
+    {
+        List<NewsViewModel> viewList = new List<NewsViewModel>();
+        HttpResponseMessage response = await _httpClient.GetAsync("/api/ListAllNews");
+
+        if (response.IsSuccessStatusCode)
         {
-            return View();
+            string data = await response.Content.ReadAsStringAsync();
+            viewList = JsonConvert.DeserializeObject<List<NewsViewModel>>(data);
+        }
+        else
+        {
+            // Log error message here
+            ModelState.AddModelError(string.Empty, "An error occurred while fetching the news list.");
         }
 
-        public IActionResult Privacy()
+        return View(viewList);
+    }
+
+    [Route("Home/Public/[action]/{id}")]
+    public async Task<IActionResult> NewsDetail(int id)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync($"api/GetNewsDetail/{id}");
+
+        if (response.IsSuccessStatusCode)
         {
-            return View();
-        }
-        [Route("/")]
-        public async Task<IActionResult> HomePageAsync()
-        {
-            List<NewsViewModel> viewList = new List<NewsViewModel>();
-            HttpResponseMessage response = await _httpClient.GetAsync("/api/ListNewsSortDESCByNewDate");
-
-            if (response.IsSuccessStatusCode)
-            {
-                string data = await response.Content.ReadAsStringAsync();
-                viewList = JsonConvert.DeserializeObject<List<NewsViewModel>>(data);
-            }
-            else
-            {
-                // Log error message here
-                ModelState.AddModelError(string.Empty, "An error occurred while fetching the news list.");
-            }
-
-            
-            return View(viewList);
-        }
-        [Route("Home/Public/[action]")]
-        public async Task<IActionResult> NewsPage()
-        {
-            List<NewsViewModel> viewList = new List<NewsViewModel>();
-            HttpResponseMessage response = await _httpClient.GetAsync("/api/ListAllNews");
-
-            if (response.IsSuccessStatusCode)
-            {
-                string data = await response.Content.ReadAsStringAsync();
-                viewList = JsonConvert.DeserializeObject<List<NewsViewModel>>(data);
-            }
-            else
-            {
-                // Log error message here
-                ModelState.AddModelError(string.Empty, "An error occurred while fetching the news list.");
-            }
-
-
-            return View(viewList);
+            var newsDetail = await response.Content.ReadFromJsonAsync<NewsViewModel>();
+            return View(newsDetail);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View("Error");
+    }
 
-        [Route("Home/Public/[action]")]
-        public IActionResult OurService()
-        {
-            return View();
-        }
-        [Route("Home/Public/[action]")]
-        public IActionResult AboutUs()
-        {
-            return View();
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [Route("Home/Public/[action]")]
+    public IActionResult OurService()
+    {
+        return View();
+    }
+
+    [Route("Home/Public/[action]")]
+    public IActionResult AboutUs()
+    {
+        return View();
     }
 }
