d5e7d6accf9af532ab395abdd01efaf6c3d53ba0
TruongNQHE140426 <truongnqhe140426@fpt.edu.vn>
2024-08-27 21:52:34 +0700

    fix: remove cate and product

--- Web/Models/ProductImageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web.Models
-{
-    public class ProductImageViewModel
-    {
-        public int Id { get; set; }
-        public int? ProductId { get; set; }
-        public string? ImageUrl { get; set; }
-    }
-}
